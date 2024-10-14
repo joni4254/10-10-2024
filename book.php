b42f0f017dbaa7e22364cd7141c56458f6e3c4b3
Dem0000 <d.jorbenadze2@gmail.com>
2024-10-14 21:40:46 +0400

    data update
    created data for book appointment section

--- book.php
+++ book.php
@@ -1,3 +1,7 @@
+
+<?php include 'data.php' ?>
+
+
 <section class="book_section layout_padding">
     <div class="container">
         <div class="row">
@@ -7,40 +11,40 @@
                         BOOK <span>APPOINTMENT</span>
                     </h4>
                     <div class="form-row">
-                        <div class="form-group col-lg-4">
-                            <label for="inputPatientName">Patient Name</label>
-                            <input type="text" class="form-control" id="inputPatientName" name="patient_name" placeholder="">
-                        </div>
-                        <div class="form-group col-lg-4">
-                            <label for="inputDoctorName">Doctor's Name</label>
-                            <select name="doctor_name" class="form-control wide" id="inputDoctorName">
-                                <option value="Doctor A">Doctor A</option>
-                                <option value="Doctor B">Doctor B</option>
-                                <option value="Doctor C">Doctor C</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-lg-4">
-                            <label for="inputDepartmentName">Department's Name</label>
-                            <select name="department_name" class="form-control wide" id="inputDepartmentName">
-                                <option value="Department A">Department A</option>
-                                <option value="Department B">Department B</option>
-                                <option value="Department C">Department C</option>
-                            </select>
-                        </div>
-                    </div>
+    <div class="form-group col-lg-4">
+        <label for="inputPatientName">Patient Name</label>
+        <input type="text" class="form-control" id="inputPatientName" name="patient_name" placeholder="">
+    </div>
+    <div class="form-group col-lg-4">
+        <label for="inputDoctorName">Doctor's Name</label>
+        <select name="doctor_name" class="form-control wide" id="inputDoctorName">
+            <?php foreach ($doctors2 as $doctor): ?>
+                <option value="<?php echo $doctor; ?>"><?php echo $doctor; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <div class="form-group col-lg-4">
+        <label for="inputDepartmentName">Department's Name</label>
+        <select name="department_name" class="form-control wide" id="inputDepartmentName">
+            <?php foreach ($departments as $department): ?>
+                <option value="<?php echo $department; ?>"><?php echo $department; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+</div>
                     <div class="form-row">
                         <div class="form-group col-lg-4">
                             <label for="inputPhone">Phone Number</label>
-                            <input type="number" class="form-control" id="inputPhone" name="phone" placeholder="XXXXXXXXXX">
+                            <input type="number" class="form-control" id="inputPhone" name="phone" placeholder="XXXXXXXXXX" required>
                         </div>
                         <div class="form-group col-lg-4">
                             <label for="inputSymptoms">Symptoms</label>
-                            <input type="text" class="form-control" id="inputSymptoms" name="symptoms" placeholder="">
+                            <input type="text" class="form-control" id="inputSymptoms" name="symptoms" placeholder="" required>
                         </div>
                         <div class="form-group col-lg-4">
                             <label for="inputDate">Choose Date</label>
                             <div class="input-group date" id="inputDate" data-date-format="mm-dd-yyyy">
-                                <input type="text" class="form-control" name="appointment_date" readonly>
+                                <input type="text" class="form-control" name="appointment_date" readonly required>
                                 <span class="input-group-addon date_icon">
                                     <i class="fa fa-calendar" aria-hidden="true"></i>
                                 </span>
